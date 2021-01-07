8c2b77b8cecc3413d247a70aed2361f19f42f4e7
Igor Peric <igorperic@live.com>
2021-01-07 23:37:28 +0100

    Modified some of the content

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 2000 ] ).flat();
     }
 
     const HeaderTitleTypeAnimation = React.memo( () => {
